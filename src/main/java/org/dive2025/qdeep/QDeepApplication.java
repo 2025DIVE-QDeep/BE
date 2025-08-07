66e8aa9812688a3a4b996a1d470479c33448fd07
KangWooJu <space1102@naver.com>
2025-08-07 15:55:18 +0900

    init : default package upload

---
+++ src/main/java/org/dive2025/qdeep/QDeepApplication.java
@@ -0,0 +1,13 @@
+package org.dive2025.qdeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QDeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QDeepApplication.class, args);
+    }
+
+}
