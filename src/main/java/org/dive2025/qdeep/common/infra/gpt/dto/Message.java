b972d2d65674493b55e1f9ea3410ea7c009bd5bf
KangWooJu <space1102@naver.com>
2025-08-21 13:50:17 +0900

    feat : api에 호출/응답을 받기 위한 DTO 생성

---
+++ src/main/java/org/dive2025/qdeep/common/infra/gpt/dto/Message.java
@@ -0,0 +1,5 @@
+package org.dive2025.qdeep.common.infra.gpt.dto;
+
+public record Message(String role,String content) {
+
+}
---
+++ src/main/java/org/dive2025/qdeep/domain/recommend/dto/response/GptClientResponse.java
@@ -0,0 +1,5 @@
+package org.dive2025.qdeep.domain.recommend.dto.response;
+
+public record GptClientResponse(String reslut,
+                                String timeStamp) {
+}
