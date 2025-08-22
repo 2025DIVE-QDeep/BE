e559c7a6227916a5d395a4b8fd5520e5104451d0
KangWooJu <space1102@naver.com>
2025-08-22 22:27:43 +0900

    refactor : 기존 응답요청 폐기

--- src/main/java/org/dive2025/qdeep/domain/recommend/dto/response/GptClientResponse.java
+++
@@ -1,6 +0,0 @@
-package org.dive2025.qdeep.domain.recommend.dto.response;
-import java.util.List;
-
-public record GptClientResponse(List<String> reslut,
-                                String timeStamp) {
-}
