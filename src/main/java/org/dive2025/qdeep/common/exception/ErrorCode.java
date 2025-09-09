2fe52fd0e710e46a98da923304bd371b28f502da
KangWooJu <space1102@naver.com>
2025-09-10 00:25:57 +0900

    refactor : file을 가져오지 못하는 에러코드 추가

--- src/main/java/org/dive2025/qdeep/common/exception/ErrorCode.java
+++ src/main/java/org/dive2025/qdeep/common/exception/ErrorCode.java
@@ -20,7 +20,8 @@ public enum ErrorCode {
     ONLY_ONCE_REVIEW_PER_USER(HttpStatus.CONFLICT,"이미 해당 유저는 글을 작성하였습니다."),
     GPT_RESPONSE_NOT_FOUND(HttpStatus.NOT_FOUND,"gpt API의 결과값을 찾을 수 없습니다."),
     UNSUITABLE_FILE_MATCHED(HttpStatus.BAD_REQUEST,"잘못된 파일 형식입니다."),
-    FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR,"파일 업로드에 실패했습니다.");
+    FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR,"파일 업로드에 실패했습니다."),
+    FILES_NOT_FOUND(HttpStatus.NOT_FOUND,"파일을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
