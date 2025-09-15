fb53e61e58943727ccdada9ea014edd1e1d9ae47
KangWooJu <space1102@naver.com>
2025-09-15 14:30:35 +0900

    refactor : 불필요한 Response DTO 삭제

--- src/main/java/org/dive2025/qdeep/domain/board/dto/response/BoardListResponse.java
+++
@@ -1,8 +0,0 @@
-package org.dive2025.qdeep.domain.board.dto.response;
-
-import org.dive2025.qdeep.domain.board.entity.Board;
-
-import java.util.List;
-
-public record BoardListResponse(List<Board> boardList) {
-}
